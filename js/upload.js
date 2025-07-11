01984346644878e691374fc64e28343dffa3cdbe
jochurru <jonatan.churruarin@outlook.com>
2025-07-11 14:51:19 -0300

    commit de arreglos en upload.js

--- js/upload.js
+++ js/upload.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBIb0ojroM5OguSOPlqG-JwxowSjIpW1PY",
   authDomain: "ducknrollremeras.firebaseapp.com",
   projectId: "ducknrollremeras",
-  storageBucket: "ducknrollremeras.appspot.com",
+  storageBucket: "ducknrollremeras.firebasestorage.app",
   messagingSenderId: "957094409993",
   appId: "1:957094409993:web:4efe5b0f845ec548a085d8"
 };
